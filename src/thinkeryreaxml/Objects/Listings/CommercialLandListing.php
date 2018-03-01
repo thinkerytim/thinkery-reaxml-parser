6c7b042b486982c75623357311ce70881aef0784
Ryan Worth <ryan@edgeonline.com.au>
2018-03-01 16:26:57 +1000

    Remove no longer necessary constructor on CommercialLandListing

--- src/thinkeryreaxml/Objects/Listings/CommercialLandListing.php
+++ src/thinkeryreaxml/Objects/Listings/CommercialLandListing.php
@@ -4,8 +4,5 @@ namespace ThinkReaXMLParser\Objects\Listings;
 
 class CommercialLandListing extends CommercialListing
 {
-    public function __construct(SimpleXMLElement $xml)
-    {
-        parent::__construct($xml);
-    }
+
 }
